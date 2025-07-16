053b132a9db783440e706e2cad6b174ed061b31f
HourBooster-ux <aali35364@gmail.com>
2025-07-16 22:19:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 const steamUser = require('steam-user');
-const steamTotp = require('steam-totp');
-const keep_alive = require('./keep_alive.js')
+const keep_alive = require('./keep_alive.js');
 
-var username = process.env.username;
-var password = process.env.password;
-var shared_secret = process.env.shared;
+const games = [730, 440, 570];
+const status = 1;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+const accounts = [];
 
+// Dynamically collect accounts from env variables
+for (let i = 1; ; i++) {
+  const username = process.env[`username${i}`];
+  const password = process.env[`password${i}`];
 
-user = new steamUser();
-user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
-user.on('loggedOn', () => {
-	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
-	user.setPersona(status);               
-	user.gamesPlayed(games);
+  if (!username || !password) break;  // stop if no more accounts
+
+  accounts.push({ username, password });
+}
+
+if (accounts.length === 0) {
+  console.error('No accounts found in environment variables.');
+  process.exit(1);
+}
+
+accounts.forEach(({ username, password }) => {
+  const user = new steamUser();
+
+  user.logOn({ accountName: username, password: password });
+
+  user.on('loggedOn', () => {
+    console.log(`${username} - Successfully logged on`);
+    user.setPersona(status);
+    user.gamesPlayed(games);
+  });
+
+  user.on('error', (err) => {
+    console.error(`${username} - Error:`, err);
+  });
 });
 
 
-// var username2 = process.env.username2;
-// var password2 = process.env.password2;
-// var shared_secret2 = process.env.shared2;
 
-// var games2 = [730, 440, 570, 304930];  // Enter here AppIDs of the needed games
-// var status2 = 1;  // 1 - online, 7 - invisible
-
-
-// user2 = new steamUser();
-// user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
-// user2.on('loggedOn', () => {
-// 	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
-// 	user2.setPersona(status2);               
-// 	user2.gamesPlayed(games2);
-// });
