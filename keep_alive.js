509cdd2cc48d16e4c18952a0ff1825d1cb1257f4
HourBooster-ux <aali35364@gmail.com>
2025-07-21 19:40:05 +0500

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const ports = [process.env.PORT || 8080, 3000];
+const ports = process.env.PORT || 8080;
 
 ports.forEach(port => {
   http.createServer((req, res) => {
